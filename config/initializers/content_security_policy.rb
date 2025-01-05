7ccb7af50beef3478deb6fea5940fa5e346db744
beemdvp <ibrahimpmahmood@gmail.com>
2025-01-05 16:26:25 +0000

    feat: rework csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,17 +9,17 @@
 require_relative '../../app/lib/content_security_policy'
 
 policy = ContentSecurityPolicy.new
-assets_host = "#{policy.assets_host} radixdlt.com *.radixdlt.com"
-media_hosts = "#{policy.media_hosts} radixdlt.com *.radixdlt.com"
+assets_host = policy.assets_host
+media_hosts = policy.media_hosts
 
 Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.font_src        :self, assets_host
-  p.img_src         :self, :data, :blob, *media_hosts
+  p.font_src        :self, assets_host, "*.radixdlt.com"
+  p.img_src         :self, :data, :blob, *media_hosts, "*.radixdlt.com"
   p.style_src       :self, assets_host, "*.radixdlt.com radixdlt.com"
-  p.media_src       :self, :data, *media_hosts
+  p.media_src       :self, :data, *media_hosts, "*.radixdlt.com"
   p.manifest_src    :self, assets_host
 
   if policy.sso_host.present?
@@ -35,13 +35,13 @@ Rails.application.config.content_security_policy do |p|
     webpacker_public_host = ENV.fetch('WEBPACKER_DEV_SERVER_PUBLIC', Webpacker.config.dev_server[:public])
     front_end_build_urls = %w(ws http).map { |protocol| "#{protocol}#{Webpacker.dev_server.https? ? 's' : ''}://#{webpacker_public_host}" }
 
-    p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url, *front_end_build_urls, "localhost:4000 localhost:3000"
-    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host
-    p.frame_src   :self, :https, :http
+    p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url, *front_end_build_urls, "localhost:4000 *.radixdlt.com"
+    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host, "localhost:4000 *.radixdlt.com"
+    p.frame_src   :self, :https, :http, "localhost:4000 *.radixdlt.com"
   else
-    p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url, "twitter.com *.twitter.com x.com *.x.com"
-    p.script_src  :self, assets_host, "'wasm-unsafe-eval'"
-    p.frame_src   :self, :https
+    p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url, "*.radixdlt.com"
+    p.script_src  :self, assets_host, "'wasm-unsafe-eval'", "*.radixdlt.com"
+    p.frame_src   :self, :https, "*.radixdlt.com"
   end
 end
 
@@ -56,8 +56,8 @@ Rails.application.config.content_security_policy_nonce_directives = %w(style-src
 
 Rails.application.reloader.to_prepare do
   PgHero::HomeController.content_security_policy do |p|
-    p.script_src :self, :unsafe_inline, assets_host
-    p.style_src  :self, :unsafe_inline, assets_host
+    p.script_src :self, :unsafe_inline, assets_host, "*.radixdlt.com radixdlt.com"
+    p.style_src  :self, :unsafe_inline, assets_host, "*.radixdlt.com radixdlt.com"
   end
 
   PgHero::HomeController.after_action do
